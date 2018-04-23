7607bba328cd09baa17a920e930fde92ca398ac5
Jessica <jessicadepaula.stm@gmail.com>
2018-04-23 02:09:35 -0300

    Modificação

--- jogo-da-memoria/src/app/app.module.ts
+++ jogo-da-memoria/src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { NewComponent } from './new/new.component';
 import { CardsComponent } from './cards/cards.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NewComponent,
     CardsComponent
   ],
   imports: [
